3f51f6f66d8d17687fcca15170c8fc63c881719f
Taha <taha.ashri.dv@gmail.com>
2025-12-25 00:06:22 +0400

    refactor: Simplify score calculation logic in ScoreManager by extracting methods for calculations

--- csharp/Tennis/ScoreManager.cs
+++ csharp/Tennis/ScoreManager.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Tennis;
 
 public class ScoreManager
@@ -8,50 +10,27 @@ public class ScoreManager
         var tempScore = 0;
         if (player1Score == player2Score)
         {
-            score = CalculateDrawScore(player1Score);
+            return CalculateDrawScore(player1Score);
         }
         if (player1Score >= 4 || player2Score >= 4)
         {
-            var minusResult = player1Score - player2Score;
-            if (minusResult == 1) score = Constants.Scores.AdvantagePlayer1;
-            else if (minusResult == -1) score = Constants.Scores.AdvantagePlayer2;
-            else if (minusResult >= 2) score = Constants.Scores.WinForPlayer1;
-            else score = Constants.Scores.WinForPlayer2;
+            return CheckResult(player1Score, player2Score);
         }
-        else
-        {
-            for (var i = 1; i < 3; i++)
-            {
-                if (i == 1) tempScore = player1Score;
-                else
-                {
-                    score += "-";
-                    tempScore = player2Score;
-                }
+        return AddScores(player1Score, player2Score);
+    }
 
-                switch (tempScore)
-                {
-                    case 0:
-                        score += Constants.Scores.Love;
-                        break;
-                    case 1:
-                        score += Constants.Scores.Fifteen;
-                        break;
-                    case 2:
-                        score += Constants.Scores.Thirty;
-                        break;
-                    case 3:
-                        score += Constants.Scores.Forty;
-                        break;
-                }
-            }
-        }
-
+    private string CheckResult(int player1Score, int player2Score)
+    {
+        string score;
+        var minusResult = player1Score - player2Score;
+        if (minusResult == 1) score = Constants.Scores.AdvantagePlayer1;
+        else if (minusResult == -1) score = Constants.Scores.AdvantagePlayer2;
+        else if (minusResult >= 2) score = Constants.Scores.WinForPlayer1;
+        else score = Constants.Scores.WinForPlayer2;
         return score;
     }
-    
-    
-    
+
+
     private string CalculateDrawScore(int playerScore)
     {
         string score = playerScore switch
@@ -61,6 +40,33 @@ public class ScoreManager
             2 => Constants.Scores.Thirty_All,
             _ => Constants.Scores.Deuce
         };
+        return score;
+    }
+    
+    
+    private string AddScores(int player1score, int player2score)
+    {
+        string score = "";
+        for (var i = 1; i < 3; i++)
+        {
+            var tempScore = 0;
+            if (i == 1) tempScore = player1score;
+            else
+            {
+                score += "-";
+                tempScore = player2score;
+            }
+           
+            score += tempScore switch
+            {
+                0 => Constants.Scores.Love,
+                1 => Constants.Scores.Fifteen,
+                2 => Constants.Scores.Thirty,
+                3 => Constants.Scores.Forty
+            };
+
+        }
+
         return score;
     }
 }
