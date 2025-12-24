bcbef563f67ea13240f79269954d22f761b29c45
Taha <taha.ashri.dv@gmail.com>
2025-12-24 23:08:55 +0400

    refactor: Add constants for tennis scoring system

---
+++ csharp/Tennis/Constants.cs
@@ -0,0 +1,20 @@
+namespace Tennis;
+
+public class Constants
+{
+    public static class Scores
+    {
+        public const string Love = "Love";
+        public const string Fifteen = "Fifteen";
+        public const string Thirty = "Thirty";
+        public const string Forty = "Forty";
+        public const string Love_All ="Love-All";
+        public const string Fifteen_All ="Fifteen-All";
+        public const string Thirty_All ="Thirty-All";
+        public const string Deuce = "Deuce";
+        public const string AdvantagePlayer1 = "Advantage player1";
+        public const string AdvantagePlayer2 = "Advantage player2";
+        public const string WinForPlayer1 = "Win for player1";
+        public const string WinForPlayer2 = "Win for player2";
+    }
+}
