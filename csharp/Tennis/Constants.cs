975031c45e4a9427a642162832d5ff60c36e5e45
Taha <taha.ashri.dv@gmail.com>
2025-12-26 14:19:55 +0400

    refactor: Update Scores class in Constants.cs to add new constants and remove unnecessary ones

--- csharp/Tennis/Constants.cs
+++ csharp/Tennis/Constants.cs
@@ -4,17 +4,18 @@ public class Constants
 {
     public static class Scores
     {
+        public const string All = "-All";
         public const string Love = "Love";
         public const string Fifteen = "Fifteen";
         public const string Thirty = "Thirty";
         public const string Forty = "Forty";
-        public const string Love_All ="Love-All";
-        public const string Fifteen_All ="Fifteen-All";
-        public const string Thirty_All ="Thirty-All";
         public const string Deuce = "Deuce";
         public const string AdvantagePlayer1 = "Advantage player1";
         public const string AdvantagePlayer2 = "Advantage player2";
         public const string WinForPlayer1 = "Win for player1";
         public const string WinForPlayer2 = "Win for player2";
+        
+        public const int WinScore = 4;
+        public const int MinDifferenceForWin = 2;
     }
 }
